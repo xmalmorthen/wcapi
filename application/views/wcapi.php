a7b7d37343ff76fe13ae71d41bcd764e665f3704
Xmal Morthen <askmeyou15@hotmail.com>
2019-01-22 16:12:37 -0600

    Init Commit

---
+++ application/views/wcapi.php
@@ -0,0 +1,39 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');?>
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+	<meta charset="utf-8">
+	<title>Firma WCAPI</title>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo base_url('assests/css/animate.css') ?>">
+	
+	<style type="text/css">
+	</style>
+</head>
+<body>
+
+<div id="container">
+	<h1>Firma WCAPI</h1>
+
+	<div class="file-upload-wrapper">
+		<input type="file" name="file" id="file">
+	</div>
+	
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.0/dist/jquery.validate.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.0/dist/additional-methods.min.js"></script>
+
+<script>
+	$(document).ready(function(){
+	'use-strict';
+
+	});
+</script>
+
+</body>
+</html>
